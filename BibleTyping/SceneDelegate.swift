dfcb37d7488475195e7022f87923f73f8f1b8097
kmjmarine <kmjmarine@naver.com>
2022-07-05 10:49:48 +0900

    first deploy

---
+++ BibleTyping/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  BibleTyping
+//
+//  Created by kmjmarine on 2022/07/05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ BibleTyping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BibleTyping
+//
+//  Created by kmjmarine on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
