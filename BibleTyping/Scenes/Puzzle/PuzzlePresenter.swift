fbc0b1971fd1ea9eb38c2ea90c43e8b77f47dcb1
kmjmarine <kmjmarine@naver.com>
2022-08-01 15:09:30 +0900

    add Puzzle random

--- BibleTyping/Scenes/Puzzle/PuzzlePresenter.swift
+++ BibleTyping/Scenes/Puzzle/PuzzlePresenter.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 protocol PuzzleProtocol: AnyObject {
-    
+    func setupViews()
+    func setup(verse: String, verse_info: String)
 }
 
 final class PuzzlePresenter: NSObject {
@@ -16,5 +17,20 @@ final class PuzzlePresenter: NSObject {
     
     init(viewController: PuzzleProtocol) {
         self.viewController = viewController
+    }
+    
+    func viewDidLoad() {
+        viewController?.setupViews()
+    }
+    
+    func viewWillAppear() {
+        let quoteList: [Puzzle]
+        quoteList = Puzzle.puzzleList
+        
+        let quote = quoteList.randomElement()
+        guard let verse = quote?.verse else { return }
+        guard let verse_info = quote?.verse_info else { return }
+        
+        viewController?.setup(verse: verse, verse_info: verse_info)
     }
 }
--- BibleTyping/Scenes/Puzzle/PuzzleViewController.swift
+++ BibleTyping/Scenes/Puzzle/PuzzleViewController.swift
@@ -10,4 +10,57 @@ import SnapKit
 
 final class PuzzleViewController: UIViewController {
     private lazy var presenter = PuzzlePresenter(viewController: self)
+    
+    private lazy var quoteLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.font = .systemFont(ofSize: 30.0, weight: .bold)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "퍼즐맞추기"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .systemYellow
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.TitleBrown ?? .systemBackground]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.TitleBrown ?? .systemBackground]
+
+        navigationController?.navigationBar.tintColor = UIColor.TitleBrown
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        presenter.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        presenter.viewWillAppear()
+    }
+}
+
+
+extension PuzzleViewController: PuzzleProtocol {
+    func setupViews() {
+        [quoteLabel]
+            .forEach{ view.addSubview($0) }
+        
+        quoteLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(24.0)
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.trailing.equalToSuperview().inset(16.0)
+        }
+    }
+    
+    func setup(verse: String, verse_info: String) {
+        quoteLabel.text = verse + verse_info
+    }
 }
